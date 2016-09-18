5f256ef116de75d2f22697a6a9f4d872061b1af7
mynewword <1562902525@qq.com>
2016-09-18 11:12:48 +0800

    two_vs_program
    two vs program

--- vs_git_2/Form1.cs
+++ vs_git_2/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace vs_git_1
+namespace vs_git_2
 {
     public partial class Form1 : Form
     {
@@ -15,7 +15,5 @@ namespace vs_git_1
         {
             InitializeComponent();
         }
-
-      
     }
 }
